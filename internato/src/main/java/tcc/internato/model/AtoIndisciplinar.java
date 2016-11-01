d30948fc0be01b2493260a036097adeb260eff04
thiagovieiraeleuterio <thiagoamamuitojesus@hotmail.com>
2016-11-01 08:01:06 -0200

    faltava esse, mas acho que vai dar problema, pois fiz primeiro o pull, mas deu conflito, com a class AtoIndisciplinar

--- internato/src/main/java/tcc/internato/model/AtoIndisciplinar.java
+++ internato/src/main/java/tcc/internato/model/AtoIndisciplinar.java
@@ -81,7 +81,7 @@ public class AtoIndisciplinar {
 
 	@ManyToMany(cascade = CascadeType.ALL)
 	@JoinTable(name = "regulamento_ato", joinColumns = @JoinColumn(name = "id_regulamento"), inverseJoinColumns = @JoinColumn(name = "id_ato"))
-	private Set<Regulamento> regulamentos = new HashSet<Regulamento>();
+	private Set<Regulamento> regulamentos;
 
 	public Long getId() {
 		return id;
