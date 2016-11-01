1e96aa7137edc159ce76512433eea05b1d715adc
Thiago Vieira <thiagoamamuitojesus@hotmail.com>
2016-11-01 13:33:57 -0200

    terminando os relacionamentos, commit do lab2

--- internato/src/main/java/tcc/internato/model/AtoIndisciplinar.java
+++ internato/src/main/java/tcc/internato/model/AtoIndisciplinar.java
@@ -1,20 +1,13 @@
 package tcc.internato.model;
 
 import java.util.Date;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.FetchType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
 //
 //
@@ -27,10 +20,10 @@ import javax.persistence.PrimaryKeyJoinColumn;
 //
 // 
 
-//@Entity
-//@Table(name = "ato_indisciplinar")
+@Entity
+@Table(name = "ato_indisciplinar")
 public class AtoIndisciplinar extends EntidadeBase {
-	@Column(name = "id_ato")
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -41,33 +34,99 @@ public class AtoIndisciplinar extends EntidadeBase {
 	@Column(name = "data_ato", nullable = false)
 	private Date data;
 
-	// RELACIONAMENTOS
-
-	// muitos atos indisciplinares estam associados a um servidor
-	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.REFRESH })
-	@PrimaryKeyJoinColumn
-	@JoinColumn(name = "servidor_fk")
-	private Servidor servidor;
-
-	// Muitos atos indisciplinares estam associados a um interno.
-	@ManyToOne
-	@JoinColumn(name = "interno_id")
-	private Interno internos;
-
-	// Um ato indisciplinar provem de uma vistoria.
-	@OneToOne(optional = false, fetch = FetchType.EAGER, cascade = { CascadeType.REFRESH })
-	@JoinColumn(name = "vistoria_fk", nullable = false, unique = true)
-	private Vistoria vistoria;
-
-	// Vários atos indisciplinares provem de uma chamada.
-	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.REFRESH })
-	@PrimaryKeyJoinColumn
-	@JoinColumn(name = "chamada_fk")
-	private Chamada chamada;
-
-	// um regulamento esta associados a 0 ou muitos atos indisciplinares
-	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "regulamento_ato", joinColumns = @JoinColumn(name = "fk_regulamento") , inverseJoinColumns = @JoinColumn(name = "id_ato") )
-	private Set<Regulamento> regulamento;
+	// // RELACIONAMENTOS
+	//
+	// // muitos atos indisciplinares estam associados a um servidor
+	// @ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.REFRESH })
+	// @PrimaryKeyJoinColumn
+	// @JoinColumn(name = "servidor_fk")
+	// private Servidor servidor;
+	//
+	// // Muitos atos indisciplinares estam associados a um interno.
+	// @ManyToOne
+	// @JoinColumn(name = "interno_id")
+	// private Interno internos;
+	//
+	// // Um ato indisciplinar provem de uma vistoria.
+	// @OneToOne(optional = false, fetch = FetchType.EAGER, cascade = {
+	// CascadeType.REFRESH })
+	// @JoinColumn(name = "vistoria_fk", nullable = false, unique = true)
+	// private Vistoria vistoria;
+	//
+	// // Vários atos indisciplinares provem de uma chamada.
+	// @ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.REFRESH })
+	// @PrimaryKeyJoinColumn
+	// @JoinColumn(name = "chamada_fk")
+	// private Chamada chamada;
+	//
+	// // um regulamento esta associados a 0 ou muitos atos indisciplinares
+	// @ManyToMany(cascade = CascadeType.ALL)
+	// @JoinTable(name = "regulamento_ato", joinColumns = @JoinColumn(name =
+	// "fk_regulamento") , inverseJoinColumns = @JoinColumn(name = "id_ato") )
+	// private Set<Regulamento> regulamento;
+	//
+	// public Long getId() {
+	// return id;
+	// }
+	//
+	// public void setId(Long id) {
+	// this.id = id;
+	// }
+	//
+	// public String getDescricao() {
+	// return descricao;
+	// }
+	//
+	// public void setDescricao(String descricao) {
+	// this.descricao = descricao;
+	// }
+	//
+	// public Date getData() {
+	// return data;
+	// }
+	//
+	// public void setData(Date data) {
+	// this.data = data;
+	// }
+	//
+	// public Servidor getServidor() {
+	// return servidor;
+	// }
+	//
+	// public void setServidor(Servidor servidor) {
+	// this.servidor = servidor;
+	// }
+	//
+	// public Interno getInternos() {
+	// return internos;
+	// }
+	//
+	// public void setInternos(Interno internos) {
+	// this.internos = internos;
+	// }
+	//
+	// public Vistoria getVistoria() {
+	// return vistoria;
+	// }
+	//
+	// public void setVistoria(Vistoria vistoria) {
+	// this.vistoria = vistoria;
+	// }
+	//
+	// public Chamada getChamada() {
+	// return chamada;
+	// }
+	//
+	// public void setChamada(Chamada chamada) {
+	// this.chamada = chamada;
+	// }
+	//
+	// public Set<Regulamento> getRegulamento() {
+	// return regulamento;
+	// }
+	//
+	// public void setRegulamento(Set<Regulamento> regulamento) {
+	// this.regulamento = regulamento;
+	// }
 
 }
